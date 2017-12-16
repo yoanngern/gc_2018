13acc24c7de2f310bccdb86ae4d89930295ec0ff
Yoann Gern <yoann@yoanngern.ch>
2017-12-16 16:58:51 +0100

    related events on page

--- wp-content/themes/gc_2018/includes/acf_fields.php
+++ wp-content/themes/gc_2018/includes/acf_fields.php
@@ -1,11 +1,14 @@
 <?php
 
-require_once( __DIR__ . '/acf/date.php' );
-require_once( __DIR__ . '/acf/event.php' );
-require_once( __DIR__ . '/acf/event_category.php' );
-require_once( __DIR__ . '/acf/header.php' );
-require_once( __DIR__ . '/acf/location.php' );
-require_once( __DIR__ . '/acf/recurrence.php' );
-require_once( __DIR__ . '/acf/service.php' );
-require_once( __DIR__ . '/acf/summary.php' );
-require_once( __DIR__ . '/acf/related_events.php' );
+if(function_exists(pll__())) {
+
+	require_once( __DIR__ . '/acf/date.php' );
+	require_once( __DIR__ . '/acf/event.php' );
+	require_once( __DIR__ . '/acf/event_category.php' );
+	require_once( __DIR__ . '/acf/header.php' );
+	require_once( __DIR__ . '/acf/location.php' );
+	require_once( __DIR__ . '/acf/recurrence.php' );
+	require_once( __DIR__ . '/acf/service.php' );
+	require_once( __DIR__ . '/acf/summary.php' );
+	require_once( __DIR__ . '/acf/related_events.php' );
+}
