c34d8ee0015a9eab9a147ee10db0e2b8a8163c5a
Yoann Gern <yoann@yoanngern.ch>
2017-12-23 12:29:26 +0100

    update homepage

--- wp-content/themes/gc_2018/functions.php
+++ wp-content/themes/gc_2018/functions.php
@@ -4,7 +4,7 @@
  *
  */
 function themeslug_enqueue_style() {
-	wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css', false );
+	wp_enqueue_style( 'core', get_template_directory_uri() . '/style_v1.css', false );
 }
 
 function themeslug_enqueue_script() {
