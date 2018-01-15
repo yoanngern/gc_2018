d32f39bdbae1a4791f87e05bbd64f7362dbbd0f1
Yoann Gern <yoann@yoanngern.ch>
2018-01-15 12:24:47 +0100

    update css

--- wp-content/themes/gc_2018/functions.php
+++ wp-content/themes/gc_2018/functions.php
@@ -4,7 +4,7 @@
  *
  */
 function themeslug_enqueue_style() {
-	wp_enqueue_style( 'core', get_template_directory_uri() . '/style_v1_2.css', false );
+	wp_enqueue_style( 'core', get_template_directory_uri() . '/style_v1_3.css', false );
 }
 
 function themeslug_enqueue_script() {
